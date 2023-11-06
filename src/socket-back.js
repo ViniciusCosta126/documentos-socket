59102cc20e6e96051ec059473636d3ee85b2ab6f
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-11-06 08:45:08 -0300

    modificando dados do banco de dados

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,46 +1,23 @@
+import { atualizaDocumento, encontrarDocumento } from "./documentosDB.js";
 import io from "./servidor.js";
-
-const documentos = [
-  {
-    nome: "JavaScript",
-    texto: "texto de javascript",
-  },
-  {
-    nome: "Node",
-    texto: "texto de Node",
-  },
-  {
-    nome: "Socket.io",
-    texto: "texto de socket.io",
-  },
-];
 
 io.on("connection", (socket) => {
   console.log("Um cliente se conectou ID:", socket.id);
 
-  socket.on("selecionar_documento", (nomeDocumento, devolveTexto) => {
+  socket.on("selecionar_documento", async (nomeDocumento, devolveTexto) => {
     socket.join(nomeDocumento);
 
-    const documento = encontrarDocumento(nomeDocumento);
+    const documento = await encontrarDocumento(nomeDocumento);
 
     if (documento) {
       devolveTexto(documento.texto);
     }
   });
 
-  socket.on("texto_editor", ({ texto, nomeDocumento }) => {
-    const documento = encontrarDocumento(nomeDocumento);
-
-    if (documento) {
-      documento.texto = texto;
+  socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
+    const atualizacao = await atualizaDocumento(nomeDocumento, texto);
+    if (atualizacao.modifiedCount) {
       socket.to(nomeDocumento).emit("texto_editor_cliente", texto);
     }
   });
 });
-
-function encontrarDocumento(nome) {
-  const documento = documentos.find((documento) => {
-    return documento.nome === nome;
-  });
-  return documento;
-}
